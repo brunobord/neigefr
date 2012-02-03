35fa484a9ff9d96d6aa0be4b06c1efd1e37be94c
Bruno Bord <brunobord@gmail.com>
2012-02-03 23:45:32 +0100

    template à peu près utilisable

--- views.py
+++ views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+# from django.views.generic import TemplateView
+from django.views.generic import ListView
+from neigefr.models import Snowflake
+from django.conf import settings
+
+
+class IndexView(ListView):
+    template_name = "index.html"
+    queryset = Snowflake.objects.all()[:100]
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(IndexView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['GOOGLE_MAPS_KEY'] = settings.GOOGLE_MAPS_KEY
+        return context
