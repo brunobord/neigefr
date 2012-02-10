8d4d6dd380cbe44b3b88bcf04263dfac41033daa
Bruno Bord <bruno@jehaisleprintemps.net>
2012-02-10 23:12:40 +0100

    Faq: closes #10

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import TemplateView
 from neigefr.views import IndexView
 
 urlpatterns = patterns('',
-    url(r'', IndexView.as_view()),
+    url(r'faq/', TemplateView.as_view(template_name="faq.html"), name='faq'),
+    url(r'', IndexView.as_view(), name='index'),
 )
