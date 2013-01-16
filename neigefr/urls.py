2e13bf465d15fefb9fd19537ad27550943d9d7f1
zyegfryed <zyegfryed@gmail.com>
2013-01-16 14:33:20 +0100

    Added admin URLs - disabled by default.

--- neigefr/urls.py
+++ neigefr/urls.py
@@ -2,8 +2,14 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
     url(r'^faq/?$', TemplateView.as_view(template_name='faq.html'), name='faq'),
     url(r'^', include('neigefr.flakes.urls')),
 )
